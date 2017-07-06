0ad67489cb1756f1d11c16efbe50eda4c976a77c
Rafael Glikis <rafaelglikis@gmail.com>
2017-07-06 17:30:34 +0300

    ArticleScraper test added

--- tests/Scrapers/ArticleScraperTest.php
+++ tests/Scrapers/ArticleScraperTest.php
@@ -1,15 +1,21 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use rafaelglikis\autogp\Scrapers\ArticleScraper;
-use examples\MyArticleScraper;
+use rafaelglikis\autogp\Scrapers\TestArticleScraper;
 
 class ArticleScraperTest extends TestCase
 {
-    private static $scraper;
-
-    public static function setUpBeforeClass()
+    public function testExtractArticle()
     {
-        ArticleScraperTest::$scraper = new MyArticleScraper();
+        $scraper = new TestArticleScraper();
+        $scraper->addArticleUrl("fixtures/test.html");
+        $scraper->extractArticles();
+
+        $article = $scraper->getArticles()[0];
+
+        $this->assertEquals($article->getTitle(),"Why people believe the myth of 'plastic rice' - BBC News");
+        $this->assertEquals($article->getImgUrl(), "https://ichef.bbci.co.uk/news/1024/cpsprodpb/8688/production/_96804443_riceball3.jpg");
+        $this->assertEquals($article->getContent(), file_get_contents("fixtures/main_content.html"));
     }
 }
+
