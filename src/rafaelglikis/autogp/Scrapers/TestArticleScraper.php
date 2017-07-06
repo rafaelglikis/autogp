39e2b4ad1d09f640d94bb115a82ccf6339892237
Rafael Glikis <rafaelglikis@gmail.com>
2017-07-06 17:45:45 +0300

    ArticleScraper test added

---
+++ src/rafaelglikis/autogp/Scrapers/TestArticleScraper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafaelglikis
+ * Date: 06/07/2017
+ * Time: 5:02 ΜΜ
+ */
+
+namespace rafaelglikis\autogp\Scrapers;
+
+
+class TestArticleScraper extends ArticleScraper
+{
+
+    public static function extractLastArticleFromCategory($categoryUrl): string
+    {
+        // TODO: Implement extractLastArticleFromCategory() method.
+    }
+}
